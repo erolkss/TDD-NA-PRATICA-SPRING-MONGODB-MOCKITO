88b35949d9fdde77d34935f9965356848d7c3d45
Lucas Lima <lucaserolima@gmail.com>
2024-03-22 17:44:16 -0400

    Firt Commit Project

---
+++ src/test/java/br/com/ero/register/RegisterApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.ero.register;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterApplicationTests {
+
+	@Test
+	void contextLoads(){}
+	
+
+}
