379f84c5dc86709509d913a25a2d986f0212d606
Lucas Lima <lucaserolima@gmail.com>
2024-03-22 17:47:06 -0400

    Write a falling test - should_register_user_successfully()

--- src/test/java/br/com/ero/register/RegisterApplicationTests.java
+++ src/test/java/br/com/ero/register/RegisterApplicationTests.java
@@ -1,12 +1,33 @@
 package br.com.ero.register;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
 
 @SpringBootTest
 class RegisterApplicationTests {
 
+	@Mock
+	private UserRespository userRespository;
+
+	@InjectMocks
+	private UserService userService;
+
 	@Test
-	void contextLoads(){}
-	
+	void should_register_user_successfully() {
+		User userBeforeSave = new User("Lucas", "1234", LocalDate.of(2001, 4, 30));
+		User userAfterSave = userBeforeSave;
+		userAfterSave.setId("1");
+		Mockito.when(userRespository.save(userBeforeSave)).thenReturn(userAfterSave);
+
+		var user = userService.register(userBeforeSave);
+
+		Mockito.verify(userRespository).save(userBeforeSave);
+		Assertions.assertEquals(userAfterSave, user);
+	}
 
 }
