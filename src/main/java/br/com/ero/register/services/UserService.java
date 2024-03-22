fc932d492551ef49c199e1fa098a24787a7f30e6
Lucas Lima <lucaserolima@gmail.com>
2024-03-22 19:45:55 -0400

    Make the test work - Modified the register() method in UserService to check age, if the age is less than 18.

--- src/main/java/br/com/ero/register/services/UserService.java
+++ src/main/java/br/com/ero/register/services/UserService.java
@@ -5,12 +5,21 @@ import br.com.ero.register.respositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
 
     public User register(User user) {
-        return userRepository.save(user);
+        var age = Period.between(user.getDataOfBirth(), LocalDate.now()).getYears();
+
+        if (age >= 18) {
+            return userRepository.save(user);
+        } else {
+            throw new RuntimeException("Idade não permitida.");
+        }
     }
 }
