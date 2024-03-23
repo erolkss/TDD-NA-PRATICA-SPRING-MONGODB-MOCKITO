c4d82fcb714bdf8ab00d41456aea9487d9c5d074
Lucas Lima <lucaserolima@gmail.com>
2024-03-23 19:03:08 -0400

    Created the 'UserControllerTest' class. Write a falling test - should_return_username_length_error()

---
+++ src/test/java/br/com/ero/register/controller/UserControllerTest.java
@@ -0,0 +1,41 @@
+package br.com.ero.register.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDate;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    @Test
+    void should_return_username_length_error() {
+        var request = new UserRequest("Lu", "1234", LocalDate.now());
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/v1/users")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(MockMvcResultMatchers
+                        .status().isBadRequest())
+                .andExpect(MockMvcResultMatchers
+                        .jsonPath("$.errors[?(@.fieldName == 'name')].message",
+                                Matchers.contains("O tamanho deve ser entre 6 e 10.")));
+    }
+}
+
