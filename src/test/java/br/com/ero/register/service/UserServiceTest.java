bad95c372ea7100ad4a422f3245e61a31fca456f
Lucas Lima <lucaserolima@gmail.com>
2024-03-22 20:00:06 -0400

    Refactor - Improve code quality - Refactoring the test, adding a parameterized test with the user's year, so it is not necessary to have two tests, but only one using the year of birth as a parameter.

--- src/test/java/br/com/ero/register/service/UserServiceTest.java
+++ src/test/java/br/com/ero/register/service/UserServiceTest.java
@@ -1,9 +1,12 @@
 package br.com.ero.register.service;
+
 import br.com.ero.register.entity.User;
 import br.com.ero.register.respositories.UserRepository;
 import br.com.ero.register.services.UserService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -14,48 +17,36 @@ import java.time.LocalDate;
 @SpringBootTest
 class UserServiceTest {
 
-	@Mock
-	private UserRepository	 userRespository;
+    @Mock
+    private UserRepository userRespository;
 
-	@InjectMocks
-	private UserService userService;
+    @InjectMocks
+    private UserService userService;
 
-	@Test
-	void should_register_user_successfully() {
-		User userBeforeSave = new User(null, "Lucas", "1234", LocalDate.of(2001, 4, 30));
-		User userAfterSave = userBeforeSave;
-		userAfterSave.setId("1");
-		Mockito.when(userRespository.save(userBeforeSave)).thenReturn(userAfterSave);
+    @Test
+    void should_register_user_successfully() {
+        User userBeforeSave = new User(null, "Lucas", "1234", LocalDate.of(2001, 4, 30));
+        User userAfterSave = userBeforeSave;
+        userAfterSave.setId("1");
+        Mockito.when(userRespository.save(userBeforeSave)).thenReturn(userAfterSave);
 
-		var user = userService.register(userBeforeSave);
+        var user = userService.register(userBeforeSave);
 
-		Mockito.verify(userRespository).save(userBeforeSave);
-		Assertions.assertEquals(userAfterSave, user);
-	}
+        Mockito.verify(userRespository).save(userBeforeSave);
+        Assertions.assertEquals(userAfterSave, user);
+    }
 
+    
+    @ParameterizedTest
+    @ValueSource(ints = {2010, 1950})
+    void should_return_error_when_age_is_invalid(Integer year) {
+        User user = new User(null, "Lucas", "1234", LocalDate.of(year, 4, 30));
 
-	@Test
-	void should_return_error_when_age_is_less_than_eighteen(){
-		User user = new User(null, "Lucas", "1234", LocalDate.of(2010, 4, 30));
+        var exception = Assertions.assertThrows(RuntimeException.class, () -> userService.register(user));
 
-		var exception = Assertions.assertThrows(RuntimeException.class, () -> userService.register(user));
+        Mockito.verifyNoInteractions(userRespository);
 
-		Mockito.verifyNoInteractions(userRespository);
-
-		Assertions.assertEquals("Idade não permitida.", exception.getMessage());
-
-	}
-
-	@Test
-	void should_return_error_when_age_is_greater_than_sixty(){
-		User user = new User(null, "Lucas", "1234", LocalDate.of(1950, 4, 30));
-
-		var exception = Assertions.assertThrows(RuntimeException.class, () -> userService.register(user));
-
-		Mockito.verifyNoInteractions(userRespository);
-
-		Assertions.assertEquals("Idade não permitida.", exception.getMessage());
-
-	}
+        Assertions.assertEquals("Idade não permitida.", exception.getMessage());
+    }
 
 }
